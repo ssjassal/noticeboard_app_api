636c5ec7e609e047d7800e83a76490a2023d9f31
527579 <soniyajassal@C02NN3P2G3QP.local>
2017-05-15 16:30:15 -0400

    testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://shielded-inlet-26562.herokuapp.com/'
+    origins 'https://shielded-inlet-26562.herokuapp.com/notices'
 
     resource '*',
       headers: :any,
