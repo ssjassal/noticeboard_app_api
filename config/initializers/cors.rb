ed2463e65bcc9dc3c9f79927f0c85c86079009ce
527579 <soniyajassal@C02NN3P2G3QP.local>
2017-05-15 16:25:44 -0400

    alter back-end

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080'
+    origins 'https://shielded-inlet-26562.herokuapp.com/'
 
     resource '*',
       headers: :any,
